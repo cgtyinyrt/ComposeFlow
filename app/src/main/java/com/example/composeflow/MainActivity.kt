75a37e3e56d4788fa4e5e945834a6f157990dde7
Çağatay İnyurt <cagatayinyurt@outlook.com>
2023-02-15 17:19:57 +0300

    update

--- app/src/main/java/com/example/composeflow/MainActivity.kt
+++ app/src/main/java/com/example/composeflow/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.composeflow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composeflow.ui.theme.ComposeFlowTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +24,73 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeFlowTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                val viewModel : MyViewModel by viewModels()
+                SecondScreen(viewModel = viewModel)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FirstScreen(viewModel: MyViewModel) {
+    val counter = viewModel.countDownTimerFlow.collectAsState(initial = 10)
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(text = counter.value.toString(),
+                fontSize = 26.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeFlowTheme {
-        Greeting("Android")
+fun SecondScreen(viewModel: MyViewModel) {
+
+    val liveDataValue = viewModel.liveData.observeAsState()
+    val stateFlowValue = viewModel.stateFlow.collectAsState()
+    val sharedFlowValue = viewModel.sharedFlow.collectAsState(initial = "")
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(modifier = Modifier.align(Alignment.Center)) {
+
+                Text(text = liveDataValue.value ?: "",
+                    fontSize = 26.sp)
+                Button(onClick = {
+                    viewModel.changeLiveDataValue()
+                }) {
+                    Text(text = "LiveData Button")
+                }
+
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                Text(text = stateFlowValue.value,
+                    fontSize = 26.sp)
+                Button(onClick = {
+                    viewModel.changeStateFlowValue()
+                }) {
+                    Text(text = "StateFlow Button")
+                }
+
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                Text(text = sharedFlowValue.value,
+                    fontSize = 26.sp)
+                Button(onClick = {
+                    viewModel.changeSharedFlowValue()
+                }) {
+                    Text(text = "SharedFlow Button")
+                }
+            }
+        }
     }
 }
